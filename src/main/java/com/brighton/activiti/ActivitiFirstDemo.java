68db72d8b4ee9db5c032f7d4fb5725fd00afef70
BrightonWang <brighton.wang@foxmail.com>
2019-06-17 00:39:10 +0800

    流程部署

--- src/main/java/com/brighton/activiti/ActivitiFirstDemo.java
+++ src/main/java/com/brighton/activiti/ActivitiFirstDemo.java
@@ -2,6 +2,9 @@ package com.brighton.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
 
 /**
  * <p>
@@ -15,7 +18,17 @@ public class ActivitiFirstDemo {
         //创建ProcessEnginConfiguration
         ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
         //通过ProcessEngineConfiguration创建ProcessEngine,此时会创建数据库
-        ProcessEngine processEngine = configuration.buildProcessEngine();
+//        ProcessEngine processEngine = configuration.buildProcessEngine();
+        //使用classpath下的activiti.cfg.xml中的配置创建processEngine(默认ProcessEngine)
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //使用processEngine创建RepositoryService
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        //部署对象
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("diagram/holiday.bpmn")
+                .addClasspathResource("diagram/holiday.png")
+                .name("请假申请流程")
+                .deploy();
         /**
             创建25张表格
          * ACT_RE_*: 'RE'表示 repository。 这个前缀的表包含了流程定义和流程静态资源 （图片，
@@ -27,6 +40,10 @@ public class ActivitiFirstDemo {
          * 等。
          * ACT_GE_*: GE 表示 general。 通用数据， 用于不同场景下。
          */
-        System.out.println(processEngine);
+        System.out.println("流程引擎: --"+processEngine);
+        System.out.println("流程部署id: --"+deployment.getId());
+        System.out.println("流程部署名称: --"+deployment.getName());
+        
+
     }
 }
